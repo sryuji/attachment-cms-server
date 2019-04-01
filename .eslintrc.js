76a3a589468473c0d0dc46f2293dd138d7753599
Ryuji <promi0904@gmail.com>
2019-04-01 16:59:25 +0900

    chore: create nestJS project

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  // 他のextendsされたルールを上書いて無効化するので順番に注意
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'typescript/prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'require-jsdoc': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    // importしたものを型宣言だけに利用してる場合に許可されるように下記で設定
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    'prettier/prettier': [
+      'error',
+      // eslint側にrule反映するため, prittierrcでなくこちらに記載
+      {
+        trailingComma: 'all',
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        useTabs: false,
+        printWidth: 120,
+      },
+      { usePrettierrc: false },
+    ],
+  },
+}
