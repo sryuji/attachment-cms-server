c9df3f97dcf6ac61e8409980aec22be27c620300
Ryuji Sakon <promi0904@gmail.com>
2021-11-28 18:36:04 +0900

    fix: Pathマッチングの不備を訂正

--- src/app/contents/serializer/contents.serializer.ts
+++ src/app/contents/serializer/contents.serializer.ts
@@ -7,7 +7,7 @@ export class ContentsSerializer extends BaseSerializer {
 
   public serialize(collection: ContentHistory[]) {
     collection.forEach((r) => {
-      const path = r.path.replace(/:word/g, 'w+').trim()
+      const path = r.path.replace(/:word/g, '\\w+').trim()
       if (!this.contents[path]) this.contents[path] = []
       const content = r.content && r.content.trim()
       this.contents[path].push({ id: r.id, selector: r.selector, content, action: r.action })
