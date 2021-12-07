9699b0bc2eb7ae75fd26b7c47caa210a1358b2b7
Ryuji Sakon <promi0904@gmail.com>
2021-12-07 23:39:08 +0900

    feat: :wordを*に変更

--- src/app/contents/serializer/contents.serializer.ts
+++ src/app/contents/serializer/contents.serializer.ts
@@ -7,7 +7,7 @@ export class ContentsSerializer extends BaseSerializer {
 
   public serialize(collection: ContentHistory[]) {
     collection.forEach((r) => {
-      const path = r.path.replace(/:word/g, '\\w+').trim()
+      const path = r.path.replace(/\*/g, '\\w+').trim()
       if (!this.contents[path]) this.contents[path] = []
       const content = r.content ? r.content.trim() : null
       this.contents[path].push({ id: r.id, type: r.type, selector: r.selector, content, action: r.action })
