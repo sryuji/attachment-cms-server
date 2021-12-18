69b7d22798c3ca6bc6692a08b5cbaf8d152f6edd
Ryuji Sakon <promi0904@gmail.com>
2021-12-18 15:30:30 +0900

    feat: add /health endpoint

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { RESPONSE_200 } from '../constant/swagger.constant'
 
+@ApiTags('root')
 @Controller()
-export class AppController {}
+export class AppController {
+  @ApiOperation({ summary: 'Health Check without db access.' })
+  @ApiResponse(RESPONSE_200)
+  @Get('/health')
+  async health() {
+    return { status: 'OK' }
+  }
+}
